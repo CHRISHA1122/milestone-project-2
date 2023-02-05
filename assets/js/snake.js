8287049e74441032893e414564f1677cd0b59b0d
CHRISHA1122 <chrisha1122@outlook.com>
2023-02-05 22:42:07 +0000

    Added snake movement

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,4 +1,4 @@
-// Phaser game configeration
+// Phaser game configuration
 var config = {
     type: Phaser.AUTO,
     width: 1280,
@@ -14,9 +14,14 @@ var config = {
     },
 };
 
+// Global variables
 var snake;
 var food;
 var cursors;
+var UP = 0;
+var DOWN = 1;
+var LEFT = 2;
+var RIGHT = 3;
 
 var game = new Phaser.Game(config);
 
@@ -29,8 +34,11 @@ function preload() {
 
 function create() {
     
+// Adds background image
     this.add.image(640, 300, 'bg');
 
+    
+// Adds food to game
     var rabbit = new Phaser.Class({
 
         Extends: Phaser.GameObjects.Image,
@@ -49,6 +57,7 @@ function create() {
     });
 
 
+// Adds snake to game
     var Snake = new Phaser.Class({
 
         initialize:
@@ -61,14 +70,83 @@ function create() {
             this.head.setOrigin(0);
 
             this.alive = true;
+            this.moveTime =0;
+            this.speed = 100;
+            this.heading = RIGHT;
+            this.direction = RIGHT;
         },
-    })
+
+        update: function (time) {
+            if (time >= this.moveTime) {
+                return this.move(time);
+            }
+        },
+
+        faceUp: function() {
+            if (this.direction === LEFT || this.direction === RIGHT) {
+                this.heading = UP;
+            }
+        },
+        faceDown: function() {
+            if (this.direction === LEFT || this.direction === RIGHT) {
+                this.heading = DOWN;
+            }
+        },
+        faceLeft: function() {
+            if (this.direction === UP || this.direction === DOWN) {
+                this.heading = LEFT;
+            }
+        },
+        faceRight: function() {
+            if (this.direction === UP || this.direction === DOWN) {
+                this.heading = RIGHT;
+            }
+        },
+
+        move: function(time) {
+            switch(this.heading) {
+                case UP: this.headPosition.y =Phaser.Math.Wrap(this.headPosition.y - 1, 0, 50);
+                break;
+                case DOWN: this.headPosition.y =Phaser.Math.Wrap(this.headPosition.y + 1, 0, 50);
+                break;
+                case LEFT: this.headPosition.x =Phaser.Math.Wrap(this.headPosition.x - 1, 0, 80);
+                break;
+                case RIGHT: this.headPosition.x =Phaser.Math.Wrap(this.headPosition.x + 1, 0, 80);
+                break;
+            }
+
+            this.direction = this.heading;
+            Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1);
+            this.moveTime = time + this.speed;
+            return true;
+        }
+    });
 
     rabbit = new rabbit(this, 16, 28);
 
     snake = new Snake(this, 8, 8);
+
+//  Create keyboard controls
+        cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {
+function update(time, delta) {
     
+    if (!snake.alive) {
+        return
+    }
+    if (cursors.up.isDown) {
+        snake.faceUp();
+    }
+    else if (cursors.down.isDown) {
+        snake.faceDown();
+    }
+    else if (cursors.left.isDown) {
+        snake.faceLeft();
+    }
+    else if (cursors.right.isDown) {
+        snake.faceRight();
+    }
+
+    snake.update(time);
 }
